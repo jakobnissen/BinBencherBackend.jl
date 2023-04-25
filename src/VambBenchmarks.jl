1e478cfbb3b9eb1be8ded813c05184a44a788c73
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2023-04-25 12:57:20 +0200

    Migrate from SnoopPrecompile to PrecompileTools

--- src/VambBenchmarks.jl
+++ src/VambBenchmarks.jl
@@ -4,7 +4,7 @@ using AbstractTrees: AbstractTrees
 using JSON3: JSON3
 using StructTypes: StructTypes
 using LazilyInitializedFields: @lazy, @isinit, @init!, @uninit!, uninit
-using SnoopPrecompile: @precompile_all_calls, @precompile_setup
+using PrecompileTools: @setup_workload, @compile_workload
 
 include("utils.jl")
 include("flags.jl")
@@ -20,12 +20,12 @@ vector(x)::Vector = x isa Vector ? x : vec(collect(x))
 imap(f) = x -> Iterators.map(f, x)
 ifilter(f) = x -> Iterators.filter(f, x)
 
-@precompile_setup let
+@setup_workload begin
     dir = joinpath(dirname(dirname(pathof(VambBenchmarks))), "files")
     refpath = joinpath(dir, "ref.json")
     binpath = joinpath(dir, "clusters.tsv")
 
-    @precompile_all_calls let
+    @compile_workload begin
         ref = open(refpath) do io
             Reference(io; min_seq_length=10)
         end
